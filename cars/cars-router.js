30b6701197646e509db06bc8791764c32ce433d7
john <john@spinningplates.org>
2020-02-04 18:55:28 -0800

    Endpoints for Update and Delete

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -5,6 +5,7 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
+// GET cars
 router.get('/', (req, res) => {
   db('cars')
     .then(cars => {
@@ -15,6 +16,25 @@ router.get('/', (req, res) => {
     });
 });
 
+// GET cars by id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('cars')
+    .where('id', id)
+    .then(account => {
+      if (account.length > 0) {
+        res.status(200).json(account);
+      } else {
+        res.status(400).json({ message: 'Invalid ID' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to get ID', err });
+    });
+});
+
+// POST new car {vin, make, model, mileage, transmission, title_status}
 router.post('/', (req, res) => {
   const accountData = req.body;
 
@@ -25,6 +45,35 @@ router.post('/', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({ error: 'Failed to post car', err });
+    });
+});
+
+// PUT updating a car {vin, make, model, mileage, transmission, title_status}
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('cars')
+    .where('id', id)
+    .update(req.body)
+    .then(updated => {
+      res.status(200).json({ updated: updated });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to update car', err });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('cars')
+    .where('id', id)
+    .del()
+    .then(deleted => {
+      res.status(200).json({ deleted: deleted });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to delete the car', err });
     });
 });
 
